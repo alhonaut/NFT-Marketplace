03a8665c0c6dcf0f9f390e85b73e138eff47ae34
alhonaut <103654086+alhonaut@users.noreply.github.com>
2024-05-02 03:56:45 +0300

    helper.hardhat.config.js

--- contract/helper.hardhat.config.js
+++ contract/helper.hardhat.config.js
@@ -2,9 +2,9 @@ const networkConfig = {
     31337: {
         name: "localhost",
     },
-    5: {
-        name: "goerli",
-        ethUsdPriceFeed: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e",
+    11155111: {
+        name: "sepolia",
+        ethUsdPriceFeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
     },
 }
 
